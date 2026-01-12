78d993d7bd9d3e29c1f87a6647fe596aa2bdc865
Balabababala <139853702+Balabababala@users.noreply.github.com>
2026-01-12 15:28:41 +0800

    fix: correct project type for Eclipse

---
+++ TaskFlowManagementSystem-backend/src/main/java/com/example/taskflow/TaskFlowManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFlowManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFlowManagementSystemBackendApplication.class, args);
+	}
+
+}
