ba720a9759bac872fcbc03e31df52e5497ffb84f
Balabababala <139853702+Balabababala@users.noreply.github.com>
2026-01-13 12:56:30 +0800

    chore: create WorkflowService interface + Impl skeleton

---
+++ TaskFlowManagementSystem-backend/src/main/java/com/example/demo/service/WorkflowService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.model.dto.WorkflowDto;
+
+	public interface WorkflowService {
+		void createWorkflow(WorkflowDto workflowDto);
+		
+		void updateWorkflow(Long id,WorkflowDto workflowDto);
+		
+		void deleteWorkflow(Long id);
+		
+		WorkflowDto findWorkflow(Long id);
+		List<WorkflowDto> findAllWorkflow();
+
+}
